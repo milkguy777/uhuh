b71f10f05c43c9fe1be0cdc827e799113a074235
Xplo-G <75330487+milkguy777@users.noreply.github.com>
2023-12-27 18:07:40 +0100

    do
    do

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
-import { storage } from "@vendetta/plugin";
+import { logger } from "@vendetta";
+import Settings from "./Settings";
 
-import settings from "./components/Settings";
-import patcher from "./stuff/patcher";
-
-export const vstorage: {
-  pinned?: Record<string, { id: string; pinned: number }[]>;
-  preferFilters?: ("server" | "local")[];
-} = storage;
-
-let unpatch: () => void;
 export default {
-  onLoad: () => (unpatch = patcher()),
-  onUnload: () => unpatch(),
-  settings,
-};
+    onLoad: () => {
+        logger.log("Hello world!");
+    },
+    onUnload: () => {
+        logger.log("Goodbye, world.");
+    },
+    settings: Settings,
+}
